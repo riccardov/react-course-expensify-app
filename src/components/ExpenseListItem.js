2a618196be0cd9056760cc15c8932c08c1701ffd
Riccardo Vandoni <rvandoni@malwarebytes.com>
2018-01-11 12:14:34 +0100

    fixed numeral item

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -9,7 +9,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3 className="list-item__title">{ description }</h3>
       <span className="list-item__subtitle">{ moment(createdAt).format('MMMM Do, YYYY') }</span>
     </div>
-    <h3 className="list-item__data">{ numeral(amount).format('$0,00') }</h3>
+    <h3 className="list-item__data">{ numeral(amount / 100).format('$0,0.00') }</h3>
   </Link>
 );
 
